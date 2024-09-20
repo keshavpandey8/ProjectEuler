89c2954e7574d87dab30373e9171324933e03a18
Keshav Pandey <113141626+keshavpandey8@users.noreply.github.com>
2024-09-20 17:26:52 -0400

    Create main.py to run and time all problem solutions

---
+++ main.py
@@ -0,0 +1,25 @@
+# Keshav Pandey
+import Problems_01_to_10
+import Problems_21_to_30
+import Problems_31_to_40
+import Problems_41_to_50
+import Problems_51_to_60
+import Problems_61_to_70
+import Problems_71_to_80
+import Problems_81_to_90
+import time;
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    Problems_01_to_10.main()
+    Problems_21_to_30.main()
+    Problems_31_to_40.main()
+    Problems_41_to_50.main()
+    Problems_51_to_60.main()
+    Problems_61_to_70.main()
+    Problems_71_to_80.main()
+    Problems_81_to_90.main()
+
+    end_time = time.time()
+    print(f"--- {end_time - start_time} seconds ---")
