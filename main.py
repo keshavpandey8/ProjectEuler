70e23c54b15c30ce4ef8e786dc84efb23b5609ad
keshavpandey8 <113141626+keshavpandey8@users.noreply.github.com>
2024-10-07 20:07:27 -0400

    Add problem solutions to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Keshav Pandey
 import Problems_01_to_10
+import Problems_11_to_20
 import Problems_21_to_30
 import Problems_31_to_40
 import Problems_41_to_50
@@ -7,12 +8,14 @@ import Problems_51_to_60
 import Problems_61_to_70
 import Problems_71_to_80
 import Problems_81_to_90
+import Problems_91_to_100
 import time;
 
 if __name__ == "__main__":
     start_time = time.time()
 
     Problems_01_to_10.main()
+    Problems_11_to_20.main()
     Problems_21_to_30.main()
     Problems_31_to_40.main()
     Problems_41_to_50.main()
@@ -20,6 +23,7 @@ if __name__ == "__main__":
     Problems_61_to_70.main()
     Problems_71_to_80.main()
     Problems_81_to_90.main()
+    Problems_91_to_100.main()
 
     end_time = time.time()
     print(f"--- {end_time - start_time} seconds ---")
